1695449bdf483920943e4e60ec926ed690fb4d76
zhangshuai <zhangshuai1@miliantech.com>
2020-07-24 17:27:31 +0800

    新增ClickSpanTextView

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.kotlinstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.kotlinstudy.view.ClickSpanTextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("helloWorld")
+
+        text_hello_world.setOnClickListener {
+            if (text_hello_world.mClickHandled){
+                // 若已处理则直接返回
+                text_hello_world.mClickHandled = false;
+                return@setOnClickListener
+            }
+            Toast.makeText(MainActivity@ this, "你点击了TextView", Toast.LENGTH_SHORT).show()
+        }
+
+        text_hello_world.setTextClickListener(object : ClickSpanTextView.TextClickListener {
+            override fun onTextClick() {
+                text_hello_world.mClickHandled = true
+                Toast.makeText(this@MainActivity, "hello world", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/kotlinstudy/view/ClickSpanTextView.kt
@@ -0,0 +1,83 @@
+package com.example.kotlinstudy.view
+
+import android.content.Context
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import com.example.kotlinstudy.R
+
+class ClickSpanTextView : AppCompatTextView {
+    private var textClickListener: TextClickListener? = null
+    private var clickText: String? = null
+    private var clickTextColor: Int = Color.BLUE
+    var mClickHandled: Boolean = false
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        val typedArray =
+            context.obtainStyledAttributes(attrs, R.styleable.ClickSpanTextView, defStyleAttr, 0)
+        clickText = typedArray.getString(R.styleable.ClickSpanTextView_clickText)
+        clickTextColor =
+            typedArray.getColor(R.styleable.ClickSpanTextView_clickTextColor, Color.BLUE)
+        typedArray?.recycle()
+
+        highlightColor = ContextCompat.getColor(context, android.R.color.transparent)
+        //这个一定要记得设置，不然点击不生效
+        movementMethod = LinkMovementMethod.getInstance()
+        generateSpan()
+    }
+
+
+    fun setClickText(clickText: String) {
+        this.clickText = clickText
+        generateSpan()
+    }
+
+
+    fun setTextClickListener(textClickListener: TextClickListener) {
+        this.textClickListener = textClickListener
+    }
+
+    interface TextClickListener {
+        fun onTextClick()
+    }
+
+    private fun generateSpan() {
+        if (TextUtils.isEmpty(text) || TextUtils.isEmpty(clickText)) {
+            return
+        }
+        val spannableString = SpannableString(text)
+        val startIndex = text.indexOf(clickText.toString())
+        val endIndex = startIndex + (clickText?.length ?: 0)
+        if (startIndex == endIndex) {
+            return
+        }
+        spannableString.setSpan(object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                textClickListener?.onTextClick()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.color = clickTextColor
+                ds.isUnderlineText = false
+                ds.clearShadowLayer()
+            }
+        }, startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        text = spannableString
+    }
+
+}
