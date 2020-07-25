fa05624665882fe5dde9f0089746f638febab4ed
zhangshuai <zhangshuai1@miliantech.com>
2020-07-25 12:13:25 +0800

    引入StatusBarUtil

--- common-ui/src/main/java/com/example/common_ui/base/BaseActivity.java
+++ common-ui/src/main/java/com/example/common_ui/base/BaseActivity.java
@@ -1,15 +1,18 @@
 package com.example.common_ui.base;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.common.utils.GlobalConfig;
 import com.example.common_ui.R;
+import com.jaeger.library.StatusBarUtil;
 
 /**
  * 基础的 activity，内部除了做 fragment 的初始化，剩下不做任何业务（当然底层肯定还会记录日志），保证所有 activity 继承时候的纯净。
@@ -20,10 +23,14 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected BaseFragment fragment;
 
+    private static final int DEFAULT_ALPHA = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
+        StatusBarUtil.setLightMode(this);
+        StatusBarUtil.setColor(this, getStatusBarColor(), DEFAULT_ALPHA);
         GlobalConfig.setCurrentActivity(this);
     }
 
@@ -73,6 +80,10 @@ public abstract class BaseActivity extends AppCompatActivity {
      */
     protected int getLayoutId() {
         return R.layout.ui_framework_activity_base;
+    }
+
+    protected int getStatusBarColor() {
+        return ContextCompat.getColor(this, R.color.white);
     }
 
     /**
