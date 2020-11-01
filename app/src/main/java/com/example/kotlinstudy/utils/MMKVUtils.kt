ca2f045f0395f7af681aa4d6a04e09e3e2f921f1
zhangshuai <zhangshuai1@miliantech.com>
2020-11-01 21:31:51 +0800

    [feature] 简单封装MMKV使用

--- app/src/main/java/com/example/kotlinstudy/utils/MMKVUtils.kt
+++ app/src/main/java/com/example/kotlinstudy/utils/MMKVUtils.kt
@@ -1,17 +1,103 @@
 package com.example.kotlinstudy.utils
 
+import com.tencent.mmkv.MMKV
+
 object MMKVUtils {
-    fun putString(key: String?, value: String?) {}
 
-    fun putBoolean(key: String?, value: Boolean?) {}
+    @JvmStatic
+    fun putString(key: String?, value: String?) {
+        notNull(key, value) {
+            MMKV.defaultMMKV().encode(key, value)
+        }
+    }
 
-    fun putInt(key: String?, value: Int?) {}
+    @JvmStatic
+    fun getString(key: String?): String {
+        notNull(key) {
+            return MMKV.defaultMMKV().decodeString(key)
+        }
+        return ""
+    }
 
-    fun putLong(key: String?, value: Long?) {}
+    @JvmStatic
+    fun putBoolean(key: String?, value: Boolean?) {
+        notNull(key, value) {
+            MMKV.defaultMMKV().encode(key, value!!)
+        }
+    }
 
-    fun putFloat(key: String?, value: Float?) {}
+    @JvmStatic
+    fun getBoolean(key: String?): Boolean {
+        notNull(key) {
+            return MMKV.defaultMMKV().decodeBool(key)
+        }
+        return false
+    }
 
-    fun putDouble(key: String?, value: Double?) {}
+    @JvmStatic
+    fun putInt(key: String?, value: Int?) {
+        notNull(key, value) {
+            MMKV.defaultMMKV().encode(key, value!!)
+        }
+    }
 
+    @JvmStatic
+    fun getInt(key: String?): Int {
+        notNull(key) {
+            return MMKV.defaultMMKV().decodeInt(key)
+        }
+        return 0
+    }
+
+    @JvmStatic
+    fun putLong(key: String?, value: Long?) {
+        notNull(key, value) {
+            MMKV.defaultMMKV().encode(key, value!!)
+        }
+    }
+
+    @JvmStatic
+    fun getLong(key: String?): Long {
+        notNull(key) {
+            return MMKV.defaultMMKV().decodeLong(key)
+        }
+        return 0
+    }
+
+    @JvmStatic
+    fun putFloat(key: String?, value: Float?) {
+        notNull(key, value) {
+            MMKV.defaultMMKV().encode(key, value!!)
+        }
+    }
+
+    @JvmStatic
+    fun getFloat(key: String?): Float {
+        notNull(key) {
+            return MMKV.defaultMMKV().decodeFloat(key)
+        }
+        return 0f
+    }
+
+    @JvmStatic
+    fun putDouble(key: String?, value: Double?) {
+        notNull(key, value) {
+            MMKV.defaultMMKV().encode(key, value!!)
+        }
+    }
+
+    @JvmStatic
+    fun getDouble(key: String?): Double {
+        notNull(key) {
+            return MMKV.defaultMMKV().decodeDouble(key)
+        }
+        return 0.0
+    }
+
+    private inline fun <R> notNull(vararg args: Any?, block: () -> R) {
+        if (args.filterNotNull().size == args.size) {
+            block()
+        }
+    }
 
 }
